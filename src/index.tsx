12cf99af1de89f21e05339cc477826eb73793caa
Julia <veluat@gmail.com>
2024-04-29 10:25:28 +0300

    feat: dependencies update

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import './index.css';
 import store from './app/redux-store'
-import ReactDOM from "react-dom";
 import {AppContainer} from "./app/App";
 import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import {createRoot} from "react-dom/client";
 
-
-ReactDOM.render(
-    <HashRouter>
-        <Provider store={store}>
-            <AppContainer/>
-        </Provider>
-    </HashRouter>,
-    document.getElementById('root'))
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <HashRouter>
+            <Provider store={store}>
+                <AppContainer/>
+            </Provider>
+        </HashRouter>
+    </StrictMode>
+)
 
 
 
